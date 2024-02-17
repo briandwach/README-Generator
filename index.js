edc7d24b6eb05ba2a20ea02c82d89c5bbc34e72d
livefromthelevel <brian.d.wach@gmail.com>
2024-02-17 14:19:07 -0600

    <Added required packages and array of questions>

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What is the title of your project?',
+    'What is the description of your project?',
+    'What are the installation instructions?',
+    'What is the usage information?',
+    'What are the contribution guidelines?',
+    'What are the test instructions?',
+    'What license are you using?',
+    'What is your GitHub username?',
+    'What is your email address?'
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();

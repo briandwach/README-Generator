f8ada6b6c419d8d4177a4b67bbf076c140302cd3
livefromthelevel <brian.d.wach@gmail.com>
2024-02-17 15:40:11 -0600

    <index.js, inquirer logic is working and prompting questions from a question array>

--- index.js
+++ index.js
@@ -4,22 +4,46 @@ const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [
-    'What is the title of your project?',
-    'What is the description of your project?',
-    'What are the installation instructions?',
-    'What is the usage information?',
-    'What are the contribution guidelines?',
-    'What are the test instructions?',
-    'What license are you using?',
-    'What is your GitHub username?',
-    'What is your email address?'
+    ['input', 'title', 'What is the title of your project?'],
+    ['input', 'description', 'What is the description of your project?'],
+    ['input', 'installation', 'What are the installation instructions?'],
+    ['input', 'usage', 'What is the usage information?'],
+    ['input', 'contribution', 'What are the contribution guidelines?'],
+    ['input', 'test', 'What are the test instructions?'],
+    ['select', 'license', 'What license are you using?'],
+    ['input', 'github', 'What is your GitHub username?'],
+    ['input', 'email', 'What is your email address?']
 ];
 
+// Inquirer function for collecting user input
+function collectInformation() {
+    let questionsArray = [];
+    for (q = 0; q < questions.length; q++) {
+        let question = questions[q];
+        let [type, name, message, choices] = question;
+        questionsArray[q] = {type: type, name: name, message: message, choices: choices};
+    };
+
+    console.log(questionsArray);
+
+    inquirer
+        .prompt(questionsArray)
+        .then((data) => {
+            console.log(data);
+        });
+};
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+    fs.writefile(fileName, data, (err) =>
+        err ? console.error(err) : console.log('README successfully generated!'))
+};
+
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+    collectInformation();
+};
 
 // Function call to initialize app
 init();

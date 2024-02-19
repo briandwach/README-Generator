4c3fa95d1a9a8d150d2712ea2c967d2f6fd4a012
livefromthelevel <brian.d.wach@gmail.com>
2024-02-19 15:58:37 -0600

    <Asyncronous javascript is working generateMarkdown.js file>

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(name, licenseData) {
-  if (name !== 'None') {
-
+function renderLicenseBadge(licenseKey, licenseLink) {
+  if (licenseKey !== 'None') {
+    return `[![License](https://img.shields.io/badge/License-${licenseKey}-blue.svg)](${licenseLink})`
   } else {
     return '';
   }
@@ -10,24 +10,38 @@ function renderLicenseBadge(name, licenseData) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(name) {
-
+async function renderLicenseLinkAsync(licenseKey) {
+  if (licenseKey !== 'None') {
+    let response = await fetch(`https://api.github.com/licenses/${licenseKey}`)
+    let licenseInfo = await response.json();
+    return licenseInfo.html_url;
+  } else {
+    return '';
+  }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(name) {
-
+function renderLicenseSection(licenseName) {
+  if (licenseName !== 'None') {
+    return `This application is covered under the following license: ${licenseName}`;
+  } else {
+    return 'This application is not covered under a license.';
+  }
 }
 
+
 // TODO: Create a function to generate markdown for README
-async function generateMarkdown(readmeContent, licenseKey) {
-  // let name = readmeContent.license;
-  console.log(licenseKey);
-  // let link = await renderLicenseLink(licenseKey);
-  //  let badge = renderLicenseBadge(readmeContent.license);
-  
-  
+async function generateMarkdownAsync(readmeContent, licenseKey) {
+  try {
+     let licenseLink = await renderLicenseLinkAsync(licenseKey);
+     var licenseBadge = renderLicenseBadge(licenseKey, licenseLink);
+     var licenseSection = renderLicenseSection(readmeContent.license);
+     console.log(licenseBadge);
+    console.log(licenseSection);
+  } catch (error) {
+    console.log('Error generating markdown:', error);
+  }
   //return `# ${readmeContent.title}`;
 }
 
